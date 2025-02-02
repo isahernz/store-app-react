c9c8edc6c27dd1f2d5e0dce10b7cb2020648951e
Brandon Hernández <isahecam@gmail.com>
2025-02-01 18:20:01 -0600

    feat(navbar): add structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,92 +23,30 @@ export const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-white shadow-lg fixed w-full top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between">
+    <nav className="bg-white shadow h-14">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 flex items-center justify-between h-full">
+        <div className="flex items-center  gap-x-12">
           <a href="/">
-            <img src="/rinban.svg" alt="Riban Logo" className="h-10" />
+            <span className="text-xl font-black text-orange-600">Rinban</span>
           </a>
-          {/* Desktop Navigation */}
-          <div className="hidden md:flex items-center space-x-8">
-            {navLinks.map((link) => (
-              <a
-                key={link.name}
-                href={link.href}
-                className="flex items-center space-x-1 text-gray-600 hover:text-indigo-600 transition-colors duration-200"
-                role="menuitem"
-              >
-                <span>{link.name}</span>
-              </a>
-            ))}
-          </div>
 
-          {/* Mobile menu button */}
-          <div className="md:hidden flex items-center">
-            <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="text-gray-600 hover:text-indigo-600 focus:outline-none"
-              aria-expanded={isMenuOpen}
-              aria-label="Toggle menu"
-            >
-              {isMenuOpen ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  aria-hidden="true"
-                  role="img"
+          <ul className="flex items-center gap-x-4">
+            {navLinks.map(({ name, href }) => (
+              <li>
+                <a
+                  href={href}
+                  className="text-sm text-[#373A40] font-quicksand"
                 >
-                  <path d="M18 6 6 18" />
-                  <path d="m6 6 12 12" />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  aria-hidden="true"
-                  role="img"
-                >
-                  <line x1="4" x2="20" y1="12" y2="12" />
-                  <line x1="4" x2="20" y1="6" y2="6" />
-                  <line x1="4" x2="20" y1="18" y2="18" />
-                </svg>
-              )}
-            </button>
-          </div>
+                  {name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex items-center gap-x-4 text-sm">
+          <button>My Cart</button>
         </div>
       </div>
-
-      {/* Mobile Navigation */}
-      {isMenuOpen && (
-        <div className="md:hidden" role="menu">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 bg-white shadow-lg">
-            {navLinks.map((link) => (
-              <a
-                key={link.name}
-                href={link.href}
-                className="flex items-center space-x-2 px-3 py-2 rounded-md text-gray-600 hover:text-indigo-600 hover:bg-gray-50 transition-colors duration-200"
-                role="menuitem"
-              >
-                <span>{link.name}</span>
-              </a>
-            ))}
-          </div>
-        </div>
-      )}
     </nav>
   );
 };
